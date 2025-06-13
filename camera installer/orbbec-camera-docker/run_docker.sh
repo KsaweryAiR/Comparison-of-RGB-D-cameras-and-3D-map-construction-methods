8fa03fcd586f09c052e978186c62e83ef8204d35
KsaweryAiR <85024534+KsaweryAiR@users.noreply.github.com>
2025-06-13 15:40:29 +0200

    dock

---
+++ camera installer/orbbec-camera-docker/run_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Zezwól lokalnie rootowi (kontenerowi) na połączenie z X11 (tylko raz, ale warto mieć w s>
+xhost +local:root
+
+# Uruchomienie kontenera z GUI i dostępem do USB
+docker run -it \
+  --rm \
+  --shm-size=1g \
+  --ulimit memlock=-1 \
+  --env="DISPLAY" \
+  --env="QT_X11_NO_MITSHM=1" \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --privileged \
+  --volume="/home/$user/idmp_shared:/root/Shared:rw" \
+  --device=/dev/usb \
+  --gpus all \
+  --env="XAUTHORITY=$XAUTH" \
+  --volume="$XAUTH:$XAUTH" \
+  --env="NVIDIA_VISIBLE_DEVICES=all" \
+  --env="NVIDIA_DRIVER_CAPABILITIES=all" \
+  --network=host \
+  --privileged \
+  --device=/dev/bus/usb \
+  -e DISPLAY=$DISPLAY \
+  --name kinect_rviz_container \
+  orbbec_rviz \
+  bash
